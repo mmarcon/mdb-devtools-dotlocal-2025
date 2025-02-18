3cd7fc7285897d4ae3dd68c45c2d41d9dd946f50
Max Marcon <massimiliano.marcon@mongodb.com>
2025-02-18 13:37:41 +0100

    Bootstrapped tests

---
+++ app/src/test/java/com/mongodb/moma/ArtworkResourceIT.java
@@ -0,0 +1,8 @@
+package com.mongodb.moma;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class ArtworkResourceIT extends ArtworkResourceTest{
+  
+}
---
+++ app/src/test/java/com/mongodb/moma/ArtworkResourceTest.java
@@ -0,0 +1,30 @@
+package com.mongodb.moma;
+
+import org.apache.http.HttpStatus;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+@QuarkusTest
+public class ArtworkResourceTest {
+  @ParameterizedTest
+  @ValueSource(strings = { "/artworks" })
+  public void testList(String path) {
+    System.out.println("Path: " + path);
+    Artwork[] artworks = RestAssured.get(path)
+            .then()
+            .contentType(ContentType.JSON)
+            .statusCode(HttpStatus.SC_OK)
+            .extract().body().as(Artwork[].class);
+
+    Assertions.assertThat(artworks).isNotNull();
+    Assertions.assertThat(artworks.length).isGreaterThan(0);
+    Assertions.assertThat(artworks[0].getTitle()).isNotBlank();
+  }
+}
